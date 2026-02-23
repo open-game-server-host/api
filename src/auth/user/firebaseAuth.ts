1b4fe7de9e3c317d414177d5fef73c5f662fe3db
Dom <dominicraebeeson@gmail.com>
2026-02-23 15:24:31 +0000

    require GOOGLE_APPLICATION_CREDENTIALS

--- src/auth/user/firebaseAuth.ts
+++ src/auth/user/firebaseAuth.ts
@@ -1,5 +1,5 @@
-import { OGSHError } from "@open-game-server-host/backend-lib";
-import firebase, { AppOptions } from "firebase-admin";
+import { OGSHError, parseEnvironmentVariables } from "@open-game-server-host/backend-lib";
+import firebase from "firebase-admin";
 import { Auth as FbAuth } from "firebase-admin/auth";
 import { Auth, AuthUid } from "./auth.js";
 
@@ -7,11 +7,12 @@ export class FirebaseAuth implements Auth {
     private readonly auth: FbAuth;
 
     constructor() {
-        // TODO get options from env
-        const options: AppOptions = {
-
-        }
-        this.auth = firebase.initializeApp(options).auth();
+        parseEnvironmentVariables([
+            {
+                key: "GOOGLE_APPLICATION_CREDENTIALS" // This is full path to the service account json file
+            }
+        ]);
+        this.auth = firebase.initializeApp().auth();
     }
 
     async validateUser(token: string): Promise<AuthUid> {
