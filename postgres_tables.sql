958b2f4006edab6a5d97bbf1fdf7b6fc5e57446b
Dom <dominicraebeeson@gmail.com>
2026-02-25 01:29:13 +0000

    snake_case => camelCase

--- postgres_tables.sql
+++ postgres_tables.sql
@@ -1,72 +1,89 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firebase_uid VARCHAR(36) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    authUid VARCHAR(36) NOT NULL UNIQUE,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE ips (
+CREATE TABLE ipv4 (
     id SERIAL PRIMARY KEY,
-    ipv4 INET,
-    ipv6 INET
+    ip INET
+);
+
+CREATE TABLE ipv6 (
+    id SERIAL PRIMARY KEY,
+    ip INET
 );
 
 CREATE TABLE regions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    iso_3166_1_a3_code CHAR(3) NOT NULL,
-    price_multiplier NUMERIC(10, 2) NOT NULL DEFAULT 1.00
+    iso31661a3code CHAR(3) NOT NULL,
+    priceMultiplier NUMERIC(10, 2) NOT NULL DEFAULT 1.00
 );
 
 CREATE TABLE daemons (
+    apiKeyHash TEXT,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    cpuArch VARCHAR(10),
+    cpuName TEXT,
     id SERIAL PRIMARY KEY,
-    ip_id INTEGER NOT NULL REFERENCES ips(id) ON DELETE RESTRICT,
-    port_range_start INTEGER NOT NULL,
-    port_range_end INTEGER NOT NULL,
-    segments INTEGER,
-    segments_used INTEGER DEFAULT 0,
-    cpu_name TEXT,
-    sftp_port INTEGER,
-    region_id INTEGER NOT NULL REFERENCES regions(id) ON DELETE RESTRICT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ipv4Id INTEGER DEFAULT NULL REFERENCES ipv4(id) ON DELETE RESTRICT,
+    ipv6Id INTEGER DEFAULT NULL REFERENCES ipv6(id) ON DELETE RESTRICT,
+    ipv4PortRangeStart INTEGER NOT NULL,
+    ipv4PortRangeEnd INTEGER NOT NULL,
+    ipv6PortRangeStart INTEGER NOT NULL,
+    ipv6PortRangeEnd INTEGER NOT NULL,
     os VARCHAR(10),
-    cpu_arch VARCHAR(50),
-    port_range_id INTEGER NOT NULL REFERENCES ip_port_ranges(id) ON DELETE RESTRICT,
+    regionId INTEGER NOT NULL REFERENCES regions(id) ON DELETE RESTRICT,
+    segments INTEGER,
+    segmentsUsed INTEGER DEFAULT 0,
 
-    CHECK (range_start <= range_end)
+    CHECK (ipv4PortRangeStart <= ipv4PortRangeEnd),
+    CHECK (ipv6PortRangeStart <= ipv6PortRangeEnd)
 );
 
 CREATE TABLE containers (
+    appId VARCHAR(30),
+    contractLengthDays INTEGER,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    daemonId INTEGER NOT NULL REFERENCES daemons(id) ON DELETE RESTRICT,
+    free BOOLEAN DEFAULT FALSE,
     id SERIAL PRIMARY KEY,
-    daemon_id INTEGER NOT NULL REFERENCES daemons(id) ON DELETE RESTRICT,
-    app VARCHAR(30),
-    variant VARCHAR(30),
-    version VARCHAR(30),
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    terminate_at TIMESTAMP,
-    contract_length_days INTEGER,
     locked BOOLEAN DEFAULT FALSE,
     name VARCHAR(30),
-    free BOOLEAN DEFAULT FALSE
+    runtime VARCHAR(20),
+    segments INTEGER,
+    terminateAt TIMESTAMP DEFAULT NULL,
+    userId INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    variantId VARCHAR(30),
+    versionId VARCHAR(30)
 );
 
 CREATE TABLE users_containers (
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    container_id INTEGER NOT NULL REFERENCES containers(id) ON DELETE CASCADE,
-    UNIQUE(user_id, container_id)
+    userId INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    containerId INTEGER NOT NULL REFERENCES containers(id) ON DELETE CASCADE,
+    UNIQUE(userId, containerId)
 );
 
-CREATE TABLE container_ports (
+CREATE TABLE container_ipv4_ports (
     id SERIAL PRIMARY KEY,
-    container_id INTEGER NOT NULL REFERENCES containers(id) ON DELETE CASCADE,
-    host_port INTEGER NOT NULL,
-    container_port INTEGER NOT NULL
+    containerId INTEGER NOT NULL REFERENCES containers(id) ON DELETE CASCADE,
+    hostPort INTEGER NOT NULL,
+    containerPort INTEGER NOT NULL
 );
 
-CREATE INDEX idx_users_firebase_uid ON users(firebase_uid);
-CREATE INDEX idx_daemons_region_id ON daemons(region_id);
-CREATE INDEX idx_containers_daemon_id ON containers(daemon_id);
-CREATE INDEX idx_containers_user_id ON containers(user_id);
-CREATE INDEX idx_users_containers_user_id ON users_containers(user_id);
-CREATE INDEX idx_users_containers_container_id ON users_containers(container_id);
-CREATE INDEX idx_container_ports_container_id ON container_ports(container_id);
+CREATE TABLE container_ipv6_ports (
+    id SERIAL PRIMARY KEY,
+    containerId INTEGER NOT NULL REFERENCES containers(id) ON DELETE CASCADE,
+    hostPort INTEGER NOT NULL,
+    containerPort INTEGER NOT NULL
+);
+
+CREATE INDEX idx_users_auth_uid ON users(authUid);
+CREATE INDEX idx_daemons_region_id ON daemons(regionId);
+CREATE INDEX idx_containers_daemon_id ON containers(daemonId);
+CREATE INDEX idx_containers_user_id ON containers(userId);
+CREATE INDEX idx_users_containers_user_id ON users_containers(userId);
+CREATE INDEX idx_users_containers_container_id ON users_containers(containerId);
+CREATE INDEX idx_container_ipv4_ports_container_id ON container_ipv4_ports(containerId);
+CREATE INDEX idx_container_ipv6_ports_container_id ON container_ipv6_ports(containerId);
