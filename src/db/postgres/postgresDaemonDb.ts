e9489b2f2a607c1e1d163d88360c75d80442156b
Dom <dominicraebeeson@gmail.com>
2026-03-01 16:06:43 +0000

    set segments_available on setup

--- src/db/postgres/postgresDaemonDb.ts
+++ src/db/postgres/postgresDaemonDb.ts
@@ -127,6 +127,7 @@ export class PostgresDaemonDb extends PostgresDb implements Partial<Database> {
                 port_range_end = $2,
                 region_id = $3,
                 segments_usable = $4,
+                segments_available = $4,
                 setup_complete = TRUE
             WHERE
                 id = $5
