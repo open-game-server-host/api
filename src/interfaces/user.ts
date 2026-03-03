edba5738ffa5cd40be2c04c06e7090262c6d9d6e
Dom <dominicraebeeson@gmail.com>
2026-03-03 15:23:23 +0000

    test

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -11,5 +11,6 @@ export type UserPermission =
 export const USER_ALL_PERMISSION: UserPermission = "*";
 
 export const USER_DEFAULT_PERMISSIONS: UserPermission[] = [
-    "createContainer"
+    "createContainer",
+    "listDaemons"
 ];
