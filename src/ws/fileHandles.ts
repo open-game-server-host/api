d7ba4f3bafbfdafae59c2b1ce7729c7f403c0753
Dom <dominicraebeeson@gmail.com>
2026-03-04 12:00:40 +0000

    wait indefinitely

--- src/ws/fileHandles.ts
+++ src/ws/fileHandles.ts
@@ -10,13 +10,13 @@ function getId(containerId: string, path: string): string {
 
 export async function waitForFileHandle(containerId: string, path: string) {
     const id = getId(containerId, path);
-    return Promise.race([
-        new Promise<number>(res => promises.set(id, res)),
-        sleep(FILE_HANDLE_WAIT_SECONDS).then(() => {
-            promises.delete(id);
-            throw new OGSHError("general/unspecified", `failed to get file handle within ${FILE_HANDLE_WAIT_SECONDS} seconds from container id '${containerId}' path '${path}'`);
-        })
-    ]);
+    return await new Promise<number>(res => promises.set(id, res));
+    // return Promise.race([
+        // sleep(FILE_HANDLE_WAIT_SECONDS).then(() => {
+            // promises.delete(id);
+            // throw new OGSHError("general/unspecified", `failed to get file handle within ${FILE_HANDLE_WAIT_SECONDS} seconds from container id '${containerId}' path '${path}'`);
+        // })
+    // ]);
 }
 
 export function fileHandleOpened(containerId: string, path: string, handle: number) {
