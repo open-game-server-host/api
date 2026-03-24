a12e286f8f12b58f36930ca5c8b2a1ed1df4b358
Dom <dominicraebeeson@gmail.com>
2026-03-24 18:11:23 +0000

    GET /v1/apps/:appId/:variantId & GET /v1/apps/:appId/:variantId/:versionId

--- src/http/routes/appHttpRoutes.ts
+++ src/http/routes/appHttpRoutes.ts
@@ -1,4 +1,4 @@
-import { getApp, getApps, respond } from "@open-game-server-host/backend-lib";
+import { getApp, getApps, getVariant, getVersion, respond } from "@open-game-server-host/backend-lib";
 import { Router } from "express";
 
 export const appHttpRouter = Router();
@@ -11,4 +11,14 @@ appHttpRouter.get("/", async (req, res) => {
 appHttpRouter.get("/:appId", async (req, res) => {
     const app = await getApp(req.params.appId);
     respond(res, app);
+});
+
+appHttpRouter.get("/:appId/:variantId", async (req, res) => {
+    const variant = await getVariant(req.params.appId, req.params.variantId);
+    respond(res, variant);
+});
+
+appHttpRouter.get("/:appId/:variantId/:versionId", async (req, res) => {
+    const version = await getVersion(req.params.appId, req.params.variantId, req.params.versionId);
+    respond(res, version);
 });
